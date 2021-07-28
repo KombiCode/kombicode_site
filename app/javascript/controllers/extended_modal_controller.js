2251f4ef2a1a7bc1b00dcb63ef41fbe25cda531f
Thierry Jet <git@kombicode>
2021-07-28 10:21:50 +0200

    Handle and customize errors in the Contact's form

--- app/javascript/controllers/extended_modal_controller.js
+++ app/javascript/controllers/extended_modal_controller.js
@@ -15,9 +15,22 @@ export default class ExtendedModal extends Modal {
     }
   }
 
+  next(event) {
+    // Hide error display area in case modal was dismissed
+    if (document.getElementById('error_explanation')) {
+      document.getElementById('error_explanation').style.display = 'none';
+    }
+  }
+
   clearErrors() {
+    console.log("Clear errors");
     if (this.hasErrorsTarget) {
       this.errorsTarget.remove()
+    }
+    // Hide error display area in case modal was dismissed
+    if (document.getElementById('error_explanation')) {
+      console.log("Clear errors explanantions");
+      document.getElementById('error_explanation').style.display = 'none';
     }
   }
 }
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,5 +4,7 @@ class Contact < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
 
+  validates :messages, presence: true
+
   accepts_nested_attributes_for :messages, reject_if: proc { |attributes| attributes['body'].blank? }
 end

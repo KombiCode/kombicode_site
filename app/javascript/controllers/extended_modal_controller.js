2b9c9506d9e3f923e5341d006738c0fafb50371b
Thierry Jet <git@kombicode>
2021-07-28 17:49:17 +0200

    Improve form reset for all cases

--- app/javascript/controllers/extended_modal_controller.js
+++ app/javascript/controllers/extended_modal_controller.js
@@ -12,38 +12,33 @@ export default class ExtendedModal extends Modal {
     // Always want to clear errors on open
     this.clearErrors();
     // Reset all values on open
-    // FIXME: need to find a clever way to do this
-    if (this.hasFormTarget) {
-      let contactFormNameElement = document.getElementById("contact_name");
-      let contactFormEmailElement = document.getElementById("contact_email");
-      let contactFormPhoneElement = document.getElementById("contact_phone");
-      let contactFormMessageElement = document.getElementById("contact_messages_attributes_0_body");
-      if (contactFormNameElement) {
-        contactFormNameElement.value = "";
-      }
-      if (contactFormEmailElement) {
-        contactFormEmailElement.value = "";
-      }
-      if (contactFormPhoneElement) {
-        contactFormPhoneElement.value = "";
-      }
-      if (contactFormMessageElement) {
-        contactFormMessageElement.value = "";
-      }
-    }
+    this.resetValues();
   }
 
   handleSuccess({ detail: { success } }) {
     if (success) {
       super.close();
-      this.clearErrors();
-      this.formTarget.reset();
     }
   }
 
   clearErrors() {
     if (this.hasErrorsTarget) {
       this.errorsTarget.remove();
+    }
+  }
+
+  resetValues() {
+    if (this.hasFormTarget) {
+      // The rest should be enough
+      this.formTarget.reset();
+      // Therefore after errors the reset seems to not work properly, so force values to be blanked
+      const container = document.querySelector("#contact_form");
+      if (container) {
+        const matches = container.querySelectorAll("input.input");
+        matches.forEach(function(userInput) {
+          userInput.value = "";
+        });
+      }
     }
   }
 }

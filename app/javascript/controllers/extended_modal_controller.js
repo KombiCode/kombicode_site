a623aed59d8c2d12ac8da8651e27ab951087bc02
Thierry Jet <git@kombicode>
2021-07-28 15:53:04 +0200

    Always clear errors in the form on open. Also reset values on open

--- app/javascript/controllers/extended_modal_controller.js
+++ app/javascript/controllers/extended_modal_controller.js
@@ -1,36 +1,49 @@
 import { Modal } from "tailwindcss-stimulus-components"
 
 export default class ExtendedModal extends Modal {
-  static targets = ["form", "errors"]
+  static targets = ["form", "errors"];
 
   connect() {
-    super.connect()
+    super.connect();
+  }
+
+  open(e) {
+    super.open(e);
+    // Always want to clear errors on open
+    this.clearErrors();
+    // Reset all values on open
+    // FIXME: need to find a clever way to do this
+    if (this.hasFormTarget) {
+      let contactFormNameElement = document.getElementById("contact_name");
+      let contactFormEmailElement = document.getElementById("contact_email");
+      let contactFormPhoneElement = document.getElementById("contact_phone");
+      let contactFormMessageElement = document.getElementById("contact_messages_attributes_0_body");
+      if (contactFormNameElement) {
+        contactFormNameElement.value = "";
+      }
+      if (contactFormEmailElement) {
+        contactFormEmailElement.value = "";
+      }
+      if (contactFormPhoneElement) {
+        contactFormPhoneElement.value = "";
+      }
+      if (contactFormMessageElement) {
+        contactFormMessageElement.value = "";
+      }
+    }
   }
 
   handleSuccess({ detail: { success } }) {
     if (success) {
-      super.close()
-      this.clearErrors()
-      this.formTarget.reset()
-    }
-  }
-
-  next(event) {
-    // Hide error display area in case modal was dismissed
-    if (document.getElementById('error_explanation')) {
-      document.getElementById('error_explanation').style.display = 'none';
+      super.close();
+      this.clearErrors();
+      this.formTarget.reset();
     }
   }
 
   clearErrors() {
-    console.log("Clear errors");
     if (this.hasErrorsTarget) {
-      this.errorsTarget.remove()
-    }
-    // Hide error display area in case modal was dismissed
-    if (document.getElementById('error_explanation')) {
-      console.log("Clear errors explanantions");
-      document.getElementById('error_explanation').style.display = 'none';
+      this.errorsTarget.remove();
     }
   }
 }

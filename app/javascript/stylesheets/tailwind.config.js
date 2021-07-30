c97c3c2cb19d273c9b53df80094ddaad3750338b
Thierry Jet <git@kombicode>
2021-07-30 23:22:37 +0200

    Use safelist inside options

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,15 +1,25 @@
 module.exports = {
-  purge: [
+  purge: {
+    content: [
     './app/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/javascript/**/*.vue'
     // Add any other JS files here (i.e. .jsx, .ts, etc...)
     ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    options: {
+      safelist: [
+        'inset-x-0',
+        'top-0',
+        'left-0',
+        'h-full'
+      ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
 }

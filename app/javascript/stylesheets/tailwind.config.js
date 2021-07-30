fd7fc813a251f3eae0db98152455ba6cd8ecb1a3
Thierry Jet <git@kombicode>
2021-07-30 21:46:57 +0200

    Add tailwind-permit.txt

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,25 +1,11 @@
 module.exports = {
-  purge: {
-    content: [
+  purge: [
     './app/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/javascript/**/*.vue'
     // Add any other JS files here (i.e. .jsx, .ts, etc...)
     ],
-    // Below are classes we explicitly want to keep no matter what
-    safelist: [
-      'hidden',
-      'animated',
-      'fadeIn',
-      'fixed',
-      'inset-0',
-      'overflow-y-auto',
-      'flex',
-      'items-center',
-      'justify-center'
-    ]
-  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},

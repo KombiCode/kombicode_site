447b859b2c1377ad6b252b6e9fc5d336b9316031
Thierry Jet <git@kombicode>
2021-07-31 00:16:23 +0200

    test deliver_now

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_message_email
     @contact = params[:contact]
-    mail(to: 'phenix.tj38@gmail.com', subject: 'New contact message')
+    mail(to: 'thierry.jet@kombicode.com', subject: 'New contact message')
   end
 end

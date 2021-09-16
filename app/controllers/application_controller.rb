d6b71206e0cd2623b9c495154b44bce5d0f83a4d
Thierry Jet <thierry.jet@kombicode.com>
2021-09-16 14:38:32 +0200

    Improve locale detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,29 +6,36 @@ class ApplicationController < ActionController::Base
 
   def switch_locale(&action)
     begin
-      locale = params[:locale] || I18n.default_locale
-      I18n.with_locale(locale, &action)
-      rescue Exception => e
-        render "errors/not_found"
+      locale = extract_locale_from_accept_language_header
+      if !I18n.available_locales.include? locale.to_sym
+        locale = I18n.default_locale
       end
+      I18n.with_locale(locale, &action)
+    rescue Exception => e
+      render "errors/not_found"
+    end
   end
-
+  
   def default_url_options
-    { locale: I18n.locale }
+    { 
+      locale: I18n.locale,
+      host: ENV["DOMAIN"] || "localhost:5000" || "localhost:3000"
+    }
   end
 
   def render_flash
     render turbo_stream: turbo_stream.update("flash_notice", partial: "shared/flash_notice")
   end
 
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:5000" || "localhost:3000" }
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  protected
+  private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
 end

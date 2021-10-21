33208d89aead0a84043b29c682d7ff04b2e96645
Thierry Jet <thierry.jet@kombicode.com>
2021-10-21 23:20:06 +0200

    avoid mail for spam emails

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,8 @@
 class ContactMailer < ApplicationMailer
+  @blacklist = ENV["SPAM_EMAILS"]
   def contact_message_email
     @contact = params[:contact]
     mail(to: 'thierry.jet@kombicode.com', subject: 'New contact message')
+      unless @blacklist.include? @contact.email
   end
 end

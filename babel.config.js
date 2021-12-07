a5badb9610feb62b20d4dd077301c2fa3e4d49d6
Thierry Jet <thierry.jet@kombicode.com>
2021-12-07 21:27:43 +0100

    Upgrade webpacker. Upgrades packages. Fix Babel warning

--- babel.config.js
+++ babel.config.js
@@ -44,10 +44,9 @@ module.exports = function(api) {
       '@babel/plugin-transform-destructuring',
       [
         '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
+        '@babel/plugin-proposal-private-methods',
+        ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+        ],
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
